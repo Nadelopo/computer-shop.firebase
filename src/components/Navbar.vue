40a20106c14c4a3cc8d0281a46bfd967666b95b4
Nadelopo <101577205+Nadelopo@users.noreply.github.com>
2022-09-20 20:19:10 +0400

    createSidebar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,8 @@ import { storeToRefs } from 'pinia'
 import firebase from 'firebase/app'
 import { useRouter } from 'vue-router'
 import Swal from 'sweetalert2'
+import { ref } from 'vue'
+import Sidebar from './Sidebar.vue'
 
 const { userEmail } = storeToRefs(usersStore())
 const router = useRouter()
@@ -42,11 +44,31 @@ const checkAuth = () => {
     })
   }
 }
+
+const open = ref(false)
 </script>
 
 <template>
   <header class="mb-8 py-1">
     <div class="container">
+      <div class="root__small">
+        <div>
+          <button class="dots" @click="open = !open">
+            <div class="dot" :class="{ 'dot__active-f': open }"></div>
+            <div class="dot" :class="{ dot__middle__active: open }"></div>
+            <div class="dot" :class="{ 'dot__active-l': open }"></div>
+          </button>
+        </div>
+        <div class="flex justify-end">
+          <router-link :to="{ name: 'Home' }">
+            <img
+              src="@/assets/img/logoChangeWhiteSizeF.png"
+              width="95"
+              alt=""
+            />
+          </router-link>
+        </div>
+      </div>
       <div class="root">
         <div class="logo">
           <router-link :to="{ name: 'Home' }">
@@ -118,6 +140,7 @@ const checkAuth = () => {
       </div>
     </div>
   </header>
+  <Sidebar v-if="open" v-model:is-open="open" />
 </template>
 
 <style scoped lang="sass">
@@ -131,6 +154,8 @@ header
   display: grid
   grid-template-columns:  180px 1fr 180px
   align-items: center
+  @media (max-width: 1023px)
+    display: none
 
 
 .nav
@@ -192,4 +217,35 @@ header
 .cart__icon
   cursor: pointer
   height: 25px
+
+.root__small
+  color: #fff
+  display: grid
+  grid-template-columns: repeat(2, 1fr)
+  align-items: center
+  @media (min-width: 1023px)
+    display: none
+  .dots
+    width: 40px
+    // height: 30px
+    display: flex
+    flex-direction: column
+    gap: 6px
+    .dot
+      transition: .25s
+      width: 100%
+      background: #fff
+      height: 3px
+      border-radius: 4px
+      &__active
+        &-f
+          transform-origin: 18%
+          transform: rotate(45deg)
+        &-l
+          transform-origin: 18%
+          transform: rotate(-45deg)
+    .dot__middle
+      opacity: 1
+      &__active
+        opacity: 0
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,109 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+})
+const emit = defineEmits(['update:isOpen'])
+
+const closeSidebar = () => {
+  removeEventListener('click', checkClick)
+  translateSidebar.value = '-320px'
+  brightness.value = '1'
+  close.value = true
+  setTimeout(() => emit('update:isOpen', false), 100)
+}
+
+const translateSidebar = ref('-320px')
+const brightness = ref('1')
+const close = ref(false)
+
+const sidebarRef = ref()
+const isMounted = ref(false)
+const checkClick = (event) => {
+  if (
+    sidebarRef.value &&
+    !event.composedPath().includes(sidebarRef.value) &&
+    isMounted.value
+  ) {
+    closeSidebar()
+  }
+  isMounted.value = true
+}
+
+onMounted(() => {
+  addEventListener('click', checkClick)
+  setTimeout(() => (translateSidebar.value = '0px'))
+  setTimeout(() => (brightness.value = '0.4'))
+})
+onUnmounted(() => removeEventListener('click', checkClick))
+</script>
+
+<template>
+  <div>
+    <div class="wrapper">
+      <div class="sidebar" ref="sidebarRef">
+        <div class="grid grid-cols-2 items-center">
+          <div>
+            <router-link :to="{ name: 'Home' }">
+              <img
+                src="@/assets/img/logoChangeWhiteSizeF.png"
+                width="95"
+                alt=""
+              />
+            </router-link>
+          </div>
+          <div class="text-end">
+            <button @click="closeSidebar">
+              <div class="dot dot-f" :class="{ 'dot-f__close': close }"></div>
+              <div class="dot dot-l" :class="{ 'dot-l__close': close }"></div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+.wrapper
+  width: 100%
+  position: absolute
+  height: 100%
+  top: 0
+  bottom: 0
+  left: 0
+  right: 0
+  transition: .2s
+  backdrop-filter: brightness(v-bind(brightness))
+  .sidebar
+    position: absolute
+    background: var(--back-main)
+    min-width: 320px
+    top: 0
+    translate: v-bind(translateSidebar)
+    height: 100%
+    color: #fff
+    padding: 20px
+    transition: .2s
+
+.dot
+  transition: .2s
+  width: 40px
+  margin: 10px 0
+  background: #fff
+  height: 3px
+  transform-origin: 27%
+  border-radius: 4px
+  &-f
+    transform: rotate(45deg)
+    &__close
+      transform: rotate(0)
+  &-l
+    transform: rotate(-45deg)
+    &__close
+      transform: rotate(0)
+</style>
